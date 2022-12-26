6be2b520a5129dca9aff080e45ec53e18e5a3849
MagdielAndres <magdiel.vampire@gmail.com>
2022-12-26 13:57:09 -0600

    Añadiendo las modificaciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proyecto001';
+
+  nombre = 'Magdiel Andrés Castellanos';
+  edad = 30;
+  email = 'magdiel.castellanos.cursoinfotec@gmail.com';
+  sueldos = [1700, 1600, 1900];
+  activo = true;
+  sitio = 'http://www.google.com';
+
+  esActivo(){
+    if(this.activo){
+      return 'Trabajador Activo';
+    }else{
+      return 'trabajador Inactivo';
+    }
+  }
+
+  ultimo3Sueldos(){
+    let suma = 0;
+    for(let i = 0;  i < this.sueldos.length; i++){
+      suma += this.sueldos[i];
+    }
+    return suma;
+  }
 }
